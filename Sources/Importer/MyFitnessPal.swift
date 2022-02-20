f150f12f2f4dc26ba94bc3d278c05467e10cfa7c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-20 11:29:35 +0500

    bump

--- Sources/Importer/MyFitnessPal.swift
+++ Sources/Importer/MyFitnessPal.swift
@@ -1,4 +1,4 @@
-public struct MyFitnessPal {
+public struct MyFitnessPalImporter {
     public static func search(for searchText: String, completion: @escaping MfpSearchCompletionHandler) {
         Store.shared.getMfpSearchResults(for: searchText, completion: completion)
     }
