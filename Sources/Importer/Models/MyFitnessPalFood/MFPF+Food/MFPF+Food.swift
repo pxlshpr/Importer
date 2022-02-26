2c3766781792225dda7e6e546a261353cb28266d
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-26 09:03:14 +0500

    tag increment

--- Sources/Importer/Models/MyFitnessPalFood/MFPF+Food/MFPF+Food.swift
+++ Sources/Importer/Models/MyFitnessPalFood/MFPF+Food/MFPF+Food.swift
@@ -75,17 +75,3 @@ extension Food.Size {
         self.amount = amount * scrapedSize.multiplier
     }
 }
-
-extension Array where Element: Hashable {
-    func removingDuplicates() -> [Element] {
-        var addedDict = [Element: Bool]()
-
-        return filter {
-            addedDict.updateValue(true, forKey: $0) == nil
-        }
-    }
-
-    mutating func removeDuplicates() {
-        self = self.removingDuplicates()
-    }
-}
