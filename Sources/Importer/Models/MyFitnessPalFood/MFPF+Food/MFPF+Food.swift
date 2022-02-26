c49eb6ee2bf1789fe83d1d0f8d9560702a08f1cd
Ahmed Khalaf <pxlshpr@gmail.com>
2022-02-26 09:20:10 +0500

    tag increment

--- Sources/Importer/Models/MyFitnessPalFood/MFPF+Food/MFPF+Food.swift
+++ Sources/Importer/Models/MyFitnessPalFood/MFPF+Food/MFPF+Food.swift
@@ -56,22 +56,70 @@ extension Food.Size {
     convenience init(scrapedSize: MyFitnessPalFood.ScrapedSize, unit: SizeUnit, amount: Double) {
         self.init()
         
-        name = scrapedSize.cleanedName.capitalized
-        
-        if scrapedSize.type == .servingWithWeight, let parsed = ServingType.parseServingWithWeight(scrapedSize.name)
-        {
-            name = parsed.name.capitalized
-        }
-        else if scrapedSize.type == .servingWithVolume, let parsed = ServingType.parseServingWithVolume(scrapedSize.name)
-        {
-            name = parsed.name.capitalized
-        }
-        else if scrapedSize.type == .servingWithServing, let parsed = ServingType.parseServingWithServing(scrapedSize.name)
-        {
-            name = parsed.serving.capitalized
-        }
-        
         self.unit = unit
         self.amount = amount * scrapedSize.multiplier
+
+        do {
+            switch scrapedSize.type {
+            case .servingWithWeight:
+                try processServingWithWeight(name: scrapedSize.name)
+            case .servingWithServing:
+                try processServingWithServing(name: scrapedSize.name)
+            case .servingWithVolume:
+                try processServingWithVolume(scrapedSize: scrapedSize, unit: unit, amount: amount)
+            default:
+                name = scrapedSize.cleanedName
+            }
+        } catch {
+            name = scrapedSize.cleanedName
+        }
+        
+        name = name.capitalized
+        
+//        let name: String
+//        if scrapedSize.type == .servingWithWeight, let parsed = ServingType.parseServingWithWeight(scrapedSize.name)
+//        {
+//            name = parsed.name
+//        }
+//        else if scrapedSize.type == .servingWithVolume, let parsed = ServingType.parseServingWithVolume(scrapedSize.name)
+//        {
+//            name = parsed.name
+//        }
+//        else if scrapedSize.type == .servingWithServing, let parsed = ServingType.parseServingWithServing(scrapedSize.name)
+//        {
+//            name = parsed.serving
+//        } else {
+//            name = scrapedSize.cleanedName
+//        }
+//
+//        self.name = name.capitalized
+//        self.unit = unit
+//        self.amount = amount * scrapedSize.multiplier
+    }
+    
+    func processServingWithWeight(name: String) throws {
+        guard let parsed = ServingType.parseServingWithWeight(name) else {
+            throw ParseError.unableToParse
+        }
+        self.name = parsed.name
+    }
+
+    func processServingWithServing(name: String) throws {
+        guard let parsed = ServingType.parseServingWithServing(name) else {
+            throw ParseError.unableToParse
+        }
+        self.name = parsed.serving
+    }
+    
+    func processServingWithVolume(scrapedSize: MyFitnessPalFood.ScrapedSize, unit: SizeUnit, amount: Double) throws {
+        guard let parsed = ServingType.parseServingWithVolume(scrapedSize.name) else {
+            throw ParseError.unableToParse
+        }
+        self.name = parsed.name
+    }
+
+    enum ParseError: Error {
+        case unableToParse
     }
 }
+
