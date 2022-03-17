601a58563701181ce235eadb02250caf8ed424b0
Ahmed Khalaf <pxlshpr@gmail.com>
2022-03-17 14:38:41 +0500

    tag increment

--- Sources/Importer/Models/Food/Food+MFPSize.swift
+++ Sources/Importer/Models/Food/Food+MFPSize.swift
@@ -3,11 +3,18 @@ import Foundation
 extension Food {
     func importMFPSizes(from mfpSizes: [MFPFood.Size], ofTypes types: [ServingType]) {
         
-        let sizesToAdd = mfpSizes
+        guard let firstSize = mfpSizes.first,
+              let firstFoodSize = Food.Size(mfpSize: firstSize, mfpSizes: mfpSizes)
+        else {
+            return
+        }
+        
+        var sizesToAdd = mfpSizes
             .dropFirst()
             .filter { types.contains($0.type) }
             .compactMap { Food.Size(mfpSize: $0, mfpSizes: mfpSizes) }
             .removingDuplicates()
+        sizesToAdd.removeAll(where: { $0 == firstFoodSize })
         
         sizes.append(contentsOf: sizesToAdd)
     }
