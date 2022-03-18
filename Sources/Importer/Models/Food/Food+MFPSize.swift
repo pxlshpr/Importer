80da5a7f381519db3c5f9df5d1c45e04f867f0ca
Ahmed Khalaf <pxlshpr@gmail.com>
2022-03-18 10:17:07 +0500

    bump

--- Sources/Importer/Models/Food/Food+MFPSize.swift
+++ Sources/Importer/Models/Food/Food+MFPSize.swift
@@ -3,9 +3,7 @@ import Foundation
 extension Food {
     func importMFPSizes(from mfpSizes: [MFPFood.Size], ofTypes types: [ServingType]) {
         
-        guard let firstSize = mfpSizes.first,
-              let firstFoodSize = Food.Size(mfpSize: firstSize, mfpSizes: mfpSizes)
-        else {
+        guard let firstSize = mfpSizes.first else {
             return
         }
         
@@ -14,7 +12,10 @@ extension Food {
             .filter { types.contains($0.type) }
             .compactMap { Food.Size(mfpSize: $0, mfpSizes: mfpSizes) }
             .removingDuplicates()
-        sizesToAdd.removeAll(where: { $0 == firstFoodSize })
+        
+        if let firstFoodSize = Food.Size(mfpSize: firstSize, mfpSizes: mfpSizes) {
+            sizesToAdd.removeAll(where: { $0 == firstFoodSize })
+        }
         
         sizes.append(contentsOf: sizesToAdd)
     }
